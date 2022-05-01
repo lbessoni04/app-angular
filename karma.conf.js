dcc5f0666bdb869f3a23a0ddb96e5aab42f6b906
SValentina <39774399+SValentina@users.noreply.github.com>
2022-05-01 16:18:17 -0300

    --no-sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    chromeOptions: {
+      args: ['--headless', '--no-sandbox']
+    }
     singleRun: false,
     restartOnFileChange: true
   });
