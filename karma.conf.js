5a7ccd73996c50a3fe3f139bdad5913323c368c8
SValentina <39774399+SValentina@users.noreply.github.com>
2022-05-02 09:22:10 -0300

    sin  '--no-sandbox'

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     chromeOptions: {
-      args: ['--headless', '--no-sandbox']
+      args: ['--headless']
     },
     singleRun: false,
     restartOnFileChange: true
