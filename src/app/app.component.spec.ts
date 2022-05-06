5955135d10140fa1604756e8533c611a382da5de
Valentina Scovenna <valentina_17_01@hotmail.com>
2022-05-06 15:47:43 -0300

    app-angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'segunda-app'`, () => {
+  it(`should have as title 'app-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('segunda-app');
+    expect(app.title).toEqual('app-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('segunda-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app-angular app is running!');
   });
 });
